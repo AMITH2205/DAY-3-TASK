19ccfd5c35cccb3a4ea8ba59eb8ed35726aecdbf
AMITH2205 <89747054+AMITH2205@users.noreply.github.com>
2022-08-25 11:11:26 +0530

    Add files via upload

---
+++ json 2.js
@@ -0,0 +1,16 @@
+var amithObj = {
+  firstName: "Amith",
+  lastName: "Raj",
+  DOB: "22-05-1999",
+  role: "developer",
+  age: "22",
+  location: "chennai",
+  nationality: "india",
+  skills: ["JS", "Python", "C++"],
+  projects: [
+    { name: "IC Engine", design: ["AUTOCADD", "ANSYS"] },
+    { name: "Hotel Booking Application", lang: ["JS", "Python"] },
+  ],
+  language: ["Tamil", "Hindi", "English"],
+};
+console.log(amithObj);
